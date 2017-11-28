af290c70481c8a20fbe9d395732e8584cdd130ea
Owner <owner@MacBook-Pro.local>
2017-11-27 20:50:37 -0500

    added axios

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const bodyParser = reuqire('body-praser');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 
 app.get('/api/check', (req, res) =>{
 
-    res.send({hi:'hello'})
+    res.send({hi:'data from server when loaded'})
 });
 
 
