98e808380c02f7eda39e2d0cec1da9ffbb20b333
Dylan McBurnett <Dylan.McBurnett@gmail.com>
2017-11-30 19:05:25 -0500

    updated schema with new users table

--- schema.sql
+++ schema.sql
@@ -19,6 +19,7 @@ DROP TABLE IF EXISTS `Users`;
 CREATE TABLE `Users` (
   `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
   `google_id` VARCHAR(255) NULL DEFAULT NULL,
+  `is_band` BINARY NULL DEFAULT NULL,
   `name` VARCHAR(255) NULL DEFAULT NULL,
   `description` TEXT(10000) NULL DEFAULT NULL,
   `email` VARCHAR(255) NULL DEFAULT NULL,
@@ -29,29 +30,6 @@ CREATE TABLE `Users` (
   `photo` VARCHAR(255) NULL DEFAULT NULL,
   `status` VARCHAR(255) NULL DEFAULT NULL,
   `credits` INTEGER(255) NULL DEFAULT 50,
-  PRIMARY KEY (`id`)
-);
-
--- ---
--- Table 'Bands'
--- 
--- ---
-
-DROP TABLE IF EXISTS `Bands`;
-		
-CREATE TABLE `Bands` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `google_id` VARCHAR(255) NULL DEFAULT NULL,
-  `name` VARCHAR(255) NULL DEFAULT NULL,
-  `description` TEXT(10000) NULL DEFAULT NULL,
-  `email` VARCHAR(255) NULL DEFAULT NULL,
-  `zip` INTEGER(20) NULL DEFAULT NULL,
-  `current_zip` INTEGER(20) NULL DEFAULT NULL,
-  `city` VARCHAR(255) NULL DEFAULT NULL,
-  `state` TEXT(50) NULL DEFAULT NULL,
-  `photo` VARCHAR(255) NULL DEFAULT NULL,
-  `status` VARCHAR(255) NULL DEFAULT NULL,
-  `credits` INTEGER(255) NULL DEFAULT 55,
   PRIMARY KEY (`id`)
 );
 
@@ -83,7 +61,7 @@ DROP TABLE IF EXISTS `Event`;
 CREATE TABLE `Event` (
   `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
   `venue_id` INTEGER NULL DEFAULT NULL,
-  `band_id` INTEGER NULL DEFAULT NULL,
+  `user_id` INTEGER NULL DEFAULT NULL,
   `name` VARCHAR(255) NULL DEFAULT NULL,
   `description` TEXT(10000) NULL DEFAULT NULL,
   `photo` VARCHAR(255) NULL DEFAULT NULL,
@@ -94,7 +72,7 @@ CREATE TABLE `Event` (
   `city` CHAR(255) NULL DEFAULT NULL,
   `state` CHAR(50) NULL DEFAULT NULL,
   `zip` INTEGER(20) NULL DEFAULT NULL,
-  `status` VARCHAR(255) NULL DEFAULT NULL,
+  `is_committed` BINARY NULL DEFAULT NULL,
   `price` INTEGER(255) NULL DEFAULT NULL,
   `min_commits` INTEGER(255) NULL DEFAULT NULL,
   `commits` INTEGER(255) NULL DEFAULT NULL,
@@ -124,7 +102,7 @@ DROP TABLE IF EXISTS `Properties`;
 		
 CREATE TABLE `Properties` (
   `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `band_id` INTEGER NULL DEFAULT NULL,
+  `user_id` INTEGER NULL DEFAULT NULL,
   `link_url` VARCHAR(255) NULL DEFAULT NULL,
   `description` TEXT(255) NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
@@ -135,7 +113,6 @@ CREATE TABLE `Properties` (
 -- ---
 
 ALTER TABLE `Users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-ALTER TABLE `Bands` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 ALTER TABLE `Venue` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 ALTER TABLE `Event` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 ALTER TABLE `Attendance` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
@@ -147,24 +124,29 @@ ALTER TABLE `Properties` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 -- ---
 
 ALTER TABLE `Event` ADD FOREIGN KEY (venue_id) REFERENCES `Venue` (`id`);
-ALTER TABLE `Event` ADD FOREIGN KEY (band_id) REFERENCES `Bands` (`id`);
+ALTER TABLE `Event` ADD FOREIGN KEY (user_id) REFERENCES `Users` (`id`);
+ALTER TABLE `Properties` ADD FOREIGN KEY (user_id) REFERENCES `Users` (`id`);
 ALTER TABLE `Attendance` ADD FOREIGN KEY (event_id) REFERENCES `Event` (`id`);
 ALTER TABLE `Attendance` ADD FOREIGN KEY (user_id) REFERENCES `Users` (`id`);
-ALTER TABLE `Properties` ADD FOREIGN KEY (band_id) REFERENCES `Bands` (`id`);
 
 -- ---
 -- Test Data
 -- ---
 
--- INSERT INTO `Users` (`id`,`google_id`,`name`,`description`,`email`,`age`,`zip`,`city`,`state`,`photo`,`status`,`credits`) VALUES
--- ('','goog1','music man','loves trombones','parade@gmail.com','33','12345','spokane','IA','pic.svg','good','');
--- INSERT INTO `Bands` (`id`,`google_id`,`name`,`description`,`email`,`zip`,`current_zip`,`city`,`state`,`photo`,`status`,`credits`) VALUES
--- ('','goog2','Monsoon Of Sound','weather music','monsoon@gmail.com','34567','','new york','NY','photo.jpeg','good','');
--- INSERT INTO `Venue` (`id`,`name`,`description`,`city`,`state`,`zip`,`location`) VALUES
--- ('','Music Hut','The last straw','Muskogee','OK','56789','here');
--- INSERT INTO `Event` (`id`,`venue_id`,`band_id`,`name`,`description`,`photo`,`start_date`,`start_time`,`end_date`,`final_commit_date`,`city`,`state`,`zip`,`status`,`price`,`min_commits`,`commits`) VALUES
--- ('','1','1','Event Fest','we got the musics','picture.png','10-10-2017','8:00pm','10-11-2017','10-09-2017','hoboken','NJ','23456','good','10','5','3');
--- INSERT INTO `Attendance` (`id`,`event_id`,`user_id`) VALUES
--- ('','4','2');
--- INSERT INTO `Properties` (`id`,`band_id`,`link_url`,`description`) VALUES
--- ('','1','www.bands.com','test band links');
+INSERT INTO `Users` (`id`,`google_id`,`is_band`, `name`,`description`,`email`,`age`,`zip`,`city`,`state`,`photo`,`status`,`credits`) VALUES
+  ('','goog1', '0','james fannerly','loves trombones','parade@gmail.com','33','12345','spokane','IA','pic.svg','','');
+
+INSERT INTO `Users` (`id`,`google_id`,`name`,`description`,`email`,`zip`,`current_zip`,`city`,`state`,`photo`,`status`,`credits`) VALUES
+('','goog2','Monsoon Of Sound', '1','weather music','monsoon@gmail.com','34567','new york','NY','photo.jpeg','','');
+
+INSERT INTO `Venue` (`id`,`name`,`description`,`city`,`state`,`zip`,`location`) VALUES
+('','Music Hut','The last straw','Muskogee','OK','56789','here');
+
+INSERT INTO `Event` (`id`,`venue_id`,`user_id`,`name`,`description`,`photo`,`start_date`,`start_time`,`end_date`,`final_commit_date`,`city`,`state`,`zip`,`is_committed`,`price`,`min_commits`,`commits`) VALUES
+('','1','2','Event Fest','we got the musics','picture.png','10-10-2017','8:00pm','10-11-2017','10-09-2017','hoboken','NJ','23456','1','10','5','7');
+
+INSERT INTO `Attendance` (`id`,`event_id`,`user_id`) VALUES
+('','1','1');
+
+INSERT INTO `Properties` (`id`,`user_id`,`link_url`,`description`) VALUES
+('','1','www.bands.com','test band links');
