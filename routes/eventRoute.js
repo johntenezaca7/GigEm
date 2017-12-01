ad85197c3f28f44875f9af8a52ae8a614ed6eaaf
Dylan McBurnett <Dylan.McBurnett@gmail.com>
2017-11-30 19:19:26 -0500

    updated addEvent route

--- routes/eventRoute.js
+++ routes/eventRoute.js
@@ -15,8 +15,8 @@ module.exports = (app, db) => {
 
   const addEvent = (req, callback) => {
     // console.log('REK',req)
-const sql = `INSERT INTO Event (venue_id,band_id,name,description,photo,start_date,start_time,end_date,final_commit_date,city,state,zip,status,price,min_commits,commits) VALUES
-  (${req.body.venue_id},${req.body.band_id},"${req.body.name}","${req.body.description}","${req.body.photo}",${req.body.start_date},"${req.body.start_time}",${req.body.end_date},${req.body.final_commit_date},"${req.body.city}","${req.body.state}",${req.body.zip},"${req.body.status}",${req.body.price},${req.body.min_commits},${req.body.commits})`;
+const sql = `INSERT INTO Event (venue_id, user_id, name, description, photo, start_date, start_time, end_date, final_commit_date, city, state, zip, is_committed, price,min_commits,commits) VALUES
+  (${req.body.venue_id},${req.body.band_id},"${req.body.name}","${req.body.description}","${req.body.photo}",${req.body.start_date},"${req.body.start_time}",${req.body.end_date},${req.body.final_commit_date},"${req.body.city}","${req.body.state}",${req.body.zip},"${req.body.is_committed}",${req.body.price},${req.body.min_commits},${req.body.commits})`;
     return db.connection.query(sql, (err, data) => {
       if (err) console.log('getAllEvent Error: ', err);
       callback(err, data);
@@ -32,24 +32,6 @@ const sql = `INSERT INTO Event (venue_id,band_id,name,description,photo,start_da
   });
   
   app.post('/addevent', (req, res) =>{
-    // console.log('REQ BODY: ', req.body);
-    // let venue_id = req.body.venue_id || null;
-    // console.log('here',venue_id)
-    // let band_id = req.body.band_id || null;
-    // let name = req.body.name || null;
-    // let description = req.body.description || null;
-    // let photo = req.body.photo || null;
-    // let start_date = req.body.start_date || null;
-    // let start_time = req.body.start_time || null;
-    // let end_date = req.body.end_date || null;
-    // let final_commit_date = req.body.final_commit_date || null;
-    // let city = req.body.city || null;
-    // let state = req.body.state || null;
-    // let zip = req.body.zip || null;
-    // let status = req.body.status || null;
-    // let price = req.body.price || null;
-    // let min_commits = req.body.min_commits || null;
-    // let commits = req.body.commits || null;
 
     addEvent(req, (err, data) => {
       if (err) res.json(err);
