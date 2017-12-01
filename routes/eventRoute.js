5eefd07a6485b9a7abed460bf52a9349b86436b7
Dylan McBurnett <Dylan.McBurnett@gmail.com>
2017-11-30 19:42:11 -0500

    updated event routes

--- routes/eventRoute.js
+++ routes/eventRoute.js
@@ -16,7 +16,7 @@ module.exports = (app, db) => {
   const addEvent = (req, callback) => {
     // console.log('REK',req)
 const sql = `INSERT INTO Event (venue_id, user_id, name, description, photo, start_date, start_time, end_date, final_commit_date, city, state, zip, is_committed, price,min_commits,commits) VALUES
-  (${req.body.venue_id},${req.body.band_id},"${req.body.name}","${req.body.description}","${req.body.photo}",${req.body.start_date},"${req.body.start_time}",${req.body.end_date},${req.body.final_commit_date},"${req.body.city}","${req.body.state}",${req.body.zip},"${req.body.is_committed}",${req.body.price},${req.body.min_commits},${req.body.commits})`;
+  (${req.body.venue_id},${req.body.user_id},"${req.body.name}","${req.body.description}","${req.body.photo}",${req.body.start_date},"${req.body.start_time}",${req.body.end_date},${req.body.final_commit_date},"${req.body.city}","${req.body.state}",${req.body.zip},"${req.body.is_committed}",${req.body.price},${req.body.min_commits},${req.body.commits})`;
     return db.connection.query(sql, (err, data) => {
       if (err) console.log('getAllEvent Error: ', err);
       callback(err, data);
@@ -32,11 +32,11 @@ const sql = `INSERT INTO Event (venue_id, user_id, name, description, photo, sta
   });
   
   app.post('/addevent', (req, res) =>{
-
+    console.log(req.body);
     addEvent(req, (err, data) => {
       if (err) res.json(err);
       
-      res.send("SUCCESSFULLY ADDED EVENT PAGE");
+      res.send(data);
     });
   });
 }
