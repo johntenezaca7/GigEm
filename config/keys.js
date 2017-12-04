74221ff4fbae6d92367b786fbcce3c9cfd1dc13f
Owner <owner@MacBook-Pro.local>
2017-12-04 15:55:33 -0500

    setting prod keys

--- config/keys.js
+++ config/keys.js
@@ -4,6 +4,7 @@ if (process.env.NODE_ENV === 'production') {
     module.exports = require('./prod')
 } else {
     //We are in development
+    console.log('WE ARE IN DEV')
     module.exports = require('./dev');
 }
 
