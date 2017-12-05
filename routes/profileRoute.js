9bed56cfda3687cd7c2e0090e9409270abce7da9
Dylan McBurnett <Dylan.McBurnett@gmail.com>
2017-12-05 15:55:46 -0500

    fix conf

--- routes/profileRoute.js
+++ routes/profileRoute.js
@@ -1,15 +1,25 @@
 const dbDef = require('../db/index');
 
 module.exports = (app, db) => {
-  app.post('/profile', (req, res) => {
-    // console.log('attempting to serve profile route');
-    console.log('BODYY',req.body);
+  app.get('/api/profile', (req, res) => {
+    console.log('attempting to serve profile route');
+    console.log(req.user.googleId);
 
-    res.send({hello:'PROFILEROUTES'})
-    // dbDef.User.findOne({where: {googleId: req.user.googleId}})
-    // .then((data) => {
-    //   console.log('found user ', console.log(data));
-    //   res.send(data);
-    // })
+    dbDef.User.findOne({where: {googleId: req.user.googleId}})
+    .then((data) => {
+      //console.log('found user ', console.log(data));
+      res.send(data);
+    })
+  })
+
+  app.post('/api/task/editprofile', async (req, res) => {
+    // console.log('req.body: ', req.body);
+    // console.log(`attempting to update with ${JSON.stringify(req.body.item)}`);
+    await dbDef.User.findOne({where: {googleId: req.user.googleId}})
+    .then((data) => {
+      data.update(
+        req.body.item
+      ).then((data) => res.send(data));
+    })
   })
 }
