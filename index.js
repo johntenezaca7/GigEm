363fc94c62bd8923dfd49524ebc47bc0365aea2e
Owner <owner@MacBook-Pro.local>
2017-12-05 08:52:42 -0500

    testing if static files are causing issue

--- index.js
+++ index.js
@@ -29,17 +29,17 @@ require('./routes/eventRoute')(app, db)
 require('./routes/profileRoute')(app, db)
 
 
-if (process.env.NODE_ENV === 'production') {
-	//Express will serve up production assets like out main.js file, or main.css file!
-	app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+// 	//Express will serve up production assets like out main.js file, or main.css file!
+// 	app.use(express.static('client/build'));
   
-	//Express will serve up the index.html file if it doesn't recognize the route!
-	const path = require('path');
-	app.get('*', (req, res) => {
-		console.log('TESTING PROD')
-		res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'));
-	});
-}
+// 	//Express will serve up the index.html file if it doesn't recognize the route!
+// 	const path = require('path');
+// 	app.get('*', (req, res) => {
+// 		console.log('TESTING PROD')
+// 		res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'));
+// 	});
+// }
 
 
 let frontRoute = function (req, res) {
