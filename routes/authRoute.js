82d20868e22e03c6176fb9c87b39efeb29538247
Owner <owner@MacBook-Pro.local>
2017-12-05 08:56:13 -0500

    adding static files

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -36,7 +36,7 @@ module.exports = (app, db) => {
     };
     
     app.get('/api/current_user', (req, res) => {
-        console.log('getting current user', req)
+        console.log('getting current user', req.user)
         res.send(req.user)
    
     });
