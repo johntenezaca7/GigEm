41625bd1d33c176e64371c4649dfcf752e6e14cf
Owner <owner@MacBook-Pro.local>
2017-12-05 11:53:36 -0500

    added scope to passport .js checking for production

--- services/passport.js
+++ services/passport.js
@@ -20,7 +20,11 @@ passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
-    proxy: true
+    proxy: true,
+    scope: [
+        "profile", "email"
+    ]
+    
   },
     async (accessToken, refreshToken, params, profile, done) => {
     
