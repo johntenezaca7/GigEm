14a130e845b5e872dd1f1f524ae426a805539af2
Dylan McBurnett <Dylan.McBurnett@gmail.com>
2017-12-06 16:04:56 -0500

    resolving files

--- client/src/Components/Band/BandPitch.jsx
+++ client/src/Components/Band/BandPitch.jsx
@@ -18,13 +18,10 @@ class BandPitch extends React.Component {
           temp: "",
           
         };
-        // this.hangleChange = this.hangleChange.bind(this);
-        // this.hangleSubmit = this.hangleSubmit.bind(this);
-        
-        // this.getInfo = this.getInfo.bind(this)
     }
 
 
+<<<<<<< 779d08ce2fa6d0bec83b85ff105aced3c73e3f5b
     
     // componentWillReceiveProps(nextProps){
     //   if (nextProps.user !== "" && this.state.temp !== nextProps.user) {
@@ -36,6 +33,24 @@ class BandPitch extends React.Component {
     //   }
     // }
 
+=======
+    componentDidMount() {
+      console.log("HEREEEEE", this.props);
+      this.props.fetchUserProfile(this.props.user)
+    }
+
+    componentWillReceiveProps(nextProps){
+      console.log("HIIIII: ", nextProps)
+      if (nextProps.user !== "" && this.state.temp !== nextProps.user) {
+        console.log("NEXT PROPS: ", nextProps.user)
+        this.props.fetchUserProfile(nextProps.user);
+        this.setState({
+          temp: nextProps.user
+        })
+      }
+    }
+
+>>>>>>> fixed reduxform package
     validateGigForm() {
       console.log('LET ME VALIDATE YOU');
     };
@@ -45,17 +60,39 @@ class BandPitch extends React.Component {
     }
 
     handleSubmit(e) {
-      console.log("USBMITTED SHOWCASE: ", this.state.temp)
+      e.preventDefault();
+      console.log("SUBMITTED SHOWCASE: ", this.state.temp)
     }
         
         render() {      
+<<<<<<< 779d08ce2fa6d0bec83b85ff105aced3c73e3f5b
       
+=======
+        // console.log("IN RENDER", this.props);
+        console.log("IN STATE FROM STORE", this.state.temp);
+>>>>>>> fixed reduxform package
         
         return (
             <div className="container container-fluid border p-3 small" >
                 <div className="row">
                 {/* <div className="container-fluid border p-3 small"> */}
                     <div className="col-sm">
+                      <form onSubmit={this.handleSubmit.bind(this)} >
+                      <div>
+                        <label>First Name</label>
+                        <div>
+                          <Field
+                            name="firstName"
+                            component="input"
+                            type="text"
+                            placeholder="First Name"
+                          />
+                        </div>
+                      </div>
+                      <div>
+                        <button type="submit" >Submit</button>
+                      </div>
+                      </form>
                         <div className="form-inline">
                           {/* <div className="col col-md-auto"> */}
                           <div>
@@ -113,6 +150,12 @@ class BandPitch extends React.Component {
     } 
 }
 
+BandPitch = reduxForm({
+  form: 'pitchGigForm'
+  // fields: ['name', 'description','photo','startDate' ,'endDate','startTime','finalCommitDate','city','state','zip','price','minCommits','bandId','venueId','venueName'],
+  // validate: validateGigForm
+})(BandPitch);
+
 function mapStateToProps(state) {
   // console.log('REDUCERS STATES IN BADPITCH:', state)
   return {
@@ -121,10 +164,14 @@ function mapStateToProps(state) {
   }
 }
 
+<<<<<<< 779d08ce2fa6d0bec83b85ff105aced3c73e3f5b
 // BandPitch = reduxForm({
 //   form: 'pitchGigForm'
 //   // fields: ['name', 'description','photo','startDate' ,'endDate','startTime','finalCommitDate','city','state','zip','price','minCommits','bandId','venueId','venueName'],
 //   // validate: validateGigForm
 // })(BandPitch);
+=======
+
+>>>>>>> fixed reduxform package
 
 export default  connect(mapStateToProps, actions)(BandPitch);
