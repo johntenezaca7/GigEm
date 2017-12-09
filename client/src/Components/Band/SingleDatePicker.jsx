a88dc1d7899b501e38e31b4cc0588631a9362450
Dylan McBurnett <Dylan.McBurnett@gmail.com>
2017-12-08 19:51:35 -0500

    final date is showing but nothing else

---
+++ client/src/Components/Band/SingleDatePicker.jsx
@@ -0,0 +1,50 @@
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
+import React from 'react';
+import { SingleDatePicker, START_DATE, END_DATE } from 'react-dates';
+
+class SingleDatePickerWrapper extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        focused: false,
+        date: null,
+      };
+  
+      this.onDateChange = this.onDateChange.bind(this);
+      this.onFocusChange = this.onFocusChange.bind(this);
+    }
+  
+    shouldComponentUpdate(nextProps, nextState) {
+      if (nextState.date === this.state.date &&
+        nextState.focused === this.state.focused) {
+        return false;
+      }
+  
+      return true;
+    }
+  
+    onDateChange(date) {
+      this.setState({ date });
+    }
+  
+    onFocusChange({ focused }) {
+      this.setState({ focused });
+    }
+  
+    render() {
+      const { focused, date } = this.state;
+  
+      return (
+        <SingleDatePicker
+          id="date_input"
+          date={date}
+          focused={focused}
+          onDateChange={this.onDateChange}
+          onFocusChange={this.onFocusChange}
+        />
+      );
+    }
+  }
+  
+  export default SingleDatePickerWrapper;
