589fe7ecdffc267a088016553a534a01d3f88e66
Dylan McBurnett <Dylan.McBurnett@gmail.com>
2017-12-11 12:23:05 -0500

    firebase DB initialized

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -5,5 +5,13 @@ module.exports = {
   port      :  '3306',
   user     : keys.dbUsername,
   password : keys.dbPassword,
-  database : 'ussvengeance_dev'
+  database : 'ussvengeance_dev',
+  firebase: {
+    apiKey: keys.firebase.apiKey,
+    authDomain: keys.firebase.authDomain,
+    databaseURL: keys.firebase.authDomain,
+    projectId: "gigem-185b4",
+    storageBucket: keys.firebase.storageBucket,
+    messagingSenderId: keys.firebase.messagingSenderId
+  }
 }
