f20b2a66d4a92d90ac95371a320ef319f488c30d
tommy.york <tommy.york@gmail.com>
2017-12-19 14:29:23 -0500

    Minor updates here and there.

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
-import createSocketIoMiddleware from 'redux-socket.io';
-import io from 'socket.io-client';
+// import createSocketIoMiddleware from 'redux-socket.io';
+// import io from 'socket.io-client';
 
 import App from './App';
 import reducers from './reducers';
