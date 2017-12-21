15288408fecf75c35732dfb32ba9758911384c51
Dylan McBurnett <Dylan.McBurnett@gmail.com>
2017-12-21 18:12:28 -0500

    cleanup

--- index.js
+++ index.js
@@ -11,41 +11,6 @@ var socket_io = require('socket.io');
 
 require('./services/passport');
 
-// const connections = [];
-// const users = [];
-
-// server.listen(3050);
-// var io = socket_io();
-// io.attach(server);
-
-// io.on('connection', function(socket){
-
-//   connections.push(socket)
-//   console.log('Connected : %s sockets connected', connections.length);
-
-//   socket.on('action', (action) => {
-// 	//   console.log('the action', action)
-// 	  if(action.type === 'server/hello'){
-
-// 	  console.log(socket.id + ' says.. ' + action.data);
-// 	  io.sockets.emit('action', {type:'message', data: action.data});
-// 	  }
-// 	})
-  
-//   socket.on('disconnect', function(data){
-// 	  connections.splice(connections.indexOf(socket), 1);
-// 	  console.log('Disconnected: %s sockets connected', connections.length)
-// 	})
-
-// 	// socket.on('send message', function(data){
-// 	// 	console.log('getting to the back', data)
-// 	// 	io.sockets.emit('new message', {msg: data})
-// 	// });
-
-// //   });
-
-
-// });
 
 var app = express();
 
