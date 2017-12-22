965b64e2452b0cccdb78aab408c15dca96acd86d
Owner <owner@MacBook-Pro.local>
2017-12-21 19:59:38 -0500

    adding heroku process env keys

--- config/prod.js
+++ config/prod.js
@@ -4,5 +4,24 @@ module.exports = {
     dbPassword: process.env.DB_PASSWORD,
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    cookieKey: process.env.COOKIE_KEY
+    cookieKey: process.env.COOKIE_KEY,
+    stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+    firebase: {
+        apiKey: process.env.FB_API_KEY,
+        authDomain: process.env.FB_AUTH_DOMAIN,
+        databaseURL: process.env.FB_DB_URL,
+        projectId: process.env.FB_PROJ_ID,
+        storageBucket: process.env.FB_STORE_BUCKET,
+        messagingSenderId: process.env.FB_MESS_SEND_ID
+    },
+    mailer: {
+    user: process.env.MAILER_U,
+    pass: process.env.MAILER_PW
+    },
+    twilioMess: {
+    accountSID: process.env.TWILIO_SID,
+    authToken: process.env.TWILIO_TOKEN
+    }
 }
+
